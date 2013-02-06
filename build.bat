76456245ab03ae93150d658d16f654b0aa82a5f2
Cremno <cremno@mail.ru>
2013-02-06 06:47:03 +0100

    build.bat: local variables and for-loop + copy

--- build.bat
+++ build.bat
@@ -1,14 +1,17 @@
 @echo off
-rem ruby create_def.rb
+if "%1" equ "mingw" (
+  call build-mingw.bat
+  exit /b
+)
+setlocal
 set MRUBY_DLL_DIR=%cd%
 set MRUBY_CONFIG=%MRUBY_DLL_DIR%\build_config.rb
 cd mruby
 ruby minirake
-if errorlevel 1 goto END
+if errorlevel 1 goto :end
 set INSTALL_SHARED_DIR=%cd%\bin\shared
-set BUILD_SHARED_DIR=%cd%\host\bin\shared
 set HOST_PATH=%cd%\build\host
-set _LIB=%LIB%
+set BUILD_SHARED_DIR=%HOST_PATH%\bin\shared
 set LIB=%LIB%;%HOST_PATH%\lib;%HOST_PATH%\tools;%HOST_PATH%\src
 md %INSTALL_SHARED_DIR% %BUILD_SHARED_DIR% 2> nul
 rc /nologo /fo %BUILD_SHARED_DIR%\mruby.res %MRUBY_DLL_DIR%\mruby.rc
@@ -17,13 +20,6 @@ link /nologo /ltcg /dll /def:%MRUBY_DLL_DIR%\mruby.def libmruby.lib mruby.res
 link /nologo /ltcg mrbc\mrbc.obj print.obj mruby.lib
 link /nologo /ltcg mruby\mruby.obj print.obj mruby.lib
 link /nologo /ltcg mirb\mirb.obj mruby.lib
-robocopy . %INSTALL_SHARED_DIR% mruby.dll mruby.lib *.exe > nul
-set LIB=%_LIB%
-set _LIB=
-set HOST_PATH=
-set BUILD_SHARED_DIR=
-set INSTALL_SHARED_DIR=
+for %%f in (mruby.dll mruby.lib *.exe) do copy %%f %INSTALL_SHARED_DIR% > nul
 :END
-set MRUBY_CONFIG=
-cd %MRUBY_DLL_DIR%
-set MRUBY_DLL_DIR=
+endlocal
