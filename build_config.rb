5cb7a97cdb40fe9d5e7dfbd6d4f4aa4a5597d024
TERAJIMA, Motoyuki <zeugschl@gmail.com>
2013-04-14 22:11:36 +0900

    Support mruby commit 97e73e126120019418d737c2d2b0f236aaaec9cb on Apr 12 19:35:26 2013 -0700
    Now, mruby and mirb are mrbgems.

--- build_config.rb
+++ build_config.rb
@@ -3,4 +3,44 @@ MRuby::Build.new do |conf|
   conf.cc.flags = %w(/c /nologo /W3 /MD /GL /O1 /DHAVE_STRING_H /DNO_GETTIMEOFDAY /D_CRT_SECURE_NO_WARNINGS)
   conf.linker.flags = %w(/nologo /LTCG)
   conf.archiver.archive_options = '/nologo /LTCG /OUT:%{outfile} %{objs}'
+
+  # Use standard Kernel#sprintf method
+  conf.gem "#{root}/mrbgems/mruby-sprintf"
+
+  # Use standard print/puts/p
+  conf.gem "#{root}/mrbgems/mruby-print"
+
+  # Use standard Math module
+  conf.gem "#{root}/mrbgems/mruby-math"
+
+  # Use standard Time class
+  conf.gem "#{root}/mrbgems/mruby-time"
+
+  # Use standard Struct class
+  conf.gem "#{root}/mrbgems/mruby-struct"
+
+  # Use extensional Enumerable module
+  conf.gem "#{root}/mrbgems/mruby-enum-ext"
+
+  # Use extensional String class
+  conf.gem "#{root}/mrbgems/mruby-string-ext"
+
+  # Use extensional Numeric class
+  conf.gem "#{root}/mrbgems/mruby-numeric-ext"
+
+  # Use extensional Array class
+  conf.gem "#{root}/mrbgems/mruby-array-ext"
+
+  # Use extensional Hash class
+  conf.gem "#{root}/mrbgems/mruby-hash-ext"
+
+  # Use Random class
+  conf.gem "#{root}/mrbgems/mruby-random"
+
+  # Generate mirb command
+  conf.gem "#{root}/mrbgems/mruby-bin-mirb"
+
+  # Generate mruby command
+  conf.gem "#{root}/mrbgems/mruby-bin-mruby"
+
 end
