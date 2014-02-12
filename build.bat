6ae7c8bd862d2a2460429646006cf0c68e7d6500
cremno <cremno@mail.ru>
2014-02-12 03:16:17 +0100

    updated (mruby/mruby@26781692b7)
    Well, there we go again. :crying_cat_face:

--- build.bat
+++ build.bat
@@ -12,12 +12,14 @@ if errorlevel 1 goto :end
 set INSTALL_SHARED_DIR=%cd%\bin\shared
 set HOST_PATH=%cd%\build\host
 set BUILD_SHARED_DIR=%HOST_PATH%\bin\shared
-set LIB=%LIB%;%HOST_PATH%\lib;%HOST_PATH%\tools;%HOST_PATH%\src
+set LIB=%LIB%;%HOST_PATH%\lib;%HOST_PATH%\tools;%HOST_PATH%\src;%HOST_PATH%\mrbgems
+rc /nologo /Iinclude /fo %BUILD_SHARED_DIR%\mruby.res %MRUBY_DLL_DIR%\mruby.rc
 md %INSTALL_SHARED_DIR% %BUILD_SHARED_DIR% 2> nul
-rc /nologo /fo %BUILD_SHARED_DIR%\mruby.res %MRUBY_DLL_DIR%\mruby.rc
 cd %BUILD_SHARED_DIR%
-link /nologo /ltcg /dll /def:%MRUBY_DLL_DIR%\mruby.def libmruby.lib mruby.res
-link /nologo /ltcg mrbc\mrbc.obj print.obj mruby.lib
+link /nologo /ltcg /machine:x86 /dll /def:%MRUBY_DLL_DIR%\mruby.def libmruby.lib mruby.res
+link /nologo /ltcg /machine:x86 mrbc\mrbc.obj print.obj mruby.lib mruby.res
+link /nologo /ltcg /machine:x86 mruby-bin-mirb\tools\mirb\mirb.obj print.obj codegen.obj string.obj mruby.lib mruby.res
+link /nologo /ltcg /machine:x86 mruby-bin-mruby\tools\mruby\mruby.obj print.obj mruby.lib mruby.res
 for %%f in (mruby.dll mruby.lib *.exe) do copy %%f %INSTALL_SHARED_DIR% > nul
 :END
 endlocal
--- build_config.rb
+++ build_config.rb
@@ -1,5 +1,5 @@
 MRuby::Build.new do |conf|
-  toolchain :vs2012
+  toolchain :visualcpp
   conf.cc.flags = %w(/c /nologo /W3 /MD /GL /O1 /DNDEBUG /DNO_GETTIMEOFDAY /D_CRT_SECURE_NO_WARNINGS)
   conf.linker.flags = %w(/nologo /LTCG)
   conf.archiver.archive_options = '/nologo /LTCG /OUT:%{outfile} %{objs}'
